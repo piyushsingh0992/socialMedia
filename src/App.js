3d490c2255133cce9fe10edf6e9dcf8ba80d4844
piyushsinghhsv <piyush.singh@hsv.digital>
2021-07-17 19:06:42 +0530

    added boilerplate code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
