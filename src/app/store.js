b3ce1f8d207dfe304470254de0ecd69e0827ac31
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-03 08:50:17 +0530

    added userSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "../container/loginContainer/authSlice";
 import postReducer from "../container/newsFeedContainer/postSlice";
 import notificationReducer from "../container/notificationContainer/notificationSlice";
+import userReducer from "../container/profileContainer/userSlice";
 export const store = configureStore({
   reducer: {
+    user: userReducer,
     auth: authSliceReducer,
-    post:postReducer,
-    notification:notificationReducer,
+    post: postReducer,
+    notification: notificationReducer,
   },
 });
--- src/container/profileContainer/userSlice.js
+++ src/container/profileContainer/userSlice.js
@@ -1,2 +1,57 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apiCall } from "../../services/apiCall";
+import { current } from "immer";
+
+export const getUserDetails = createAsyncThunk(
+  "user/getUserDetails",
+
+  async (userId, { fulfillWithValue, rejectWithValue }) => {
+    let response = await apiCall("GET", `notification`);
+
+    if (response.success) {
+      return fulfillWithValue(response);
+    } else {
+      return rejectWithValue(response);
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userDetails: [],
+    status: "idle",
+  },
+
+  reducers: {
+    populatingUserDetails: (state, action) => {
+      return {
+        userDetails: action.payload.userDetails,
+        status: "fullfilled",
+      };
+    },
+
+    cleartUserDetails: (state) => {
+      return {
+        userDetails: [],
+        status: "idle",
+      };
+    },
+  },
+  extraReducers: {
+    [getUserDetails.pending]: (state) => {
+      state.status = "loading";
+    },
+    [getUserDetails.fulfilled]: (state, action) => {
+      state.notifications = action.payload.userDetails;
+      state.status = "fullfilled";
+    },
+    [getUserDetails.rejected]: (state, action) => {
+      state.status = "rejected";
+    },
+  },
+});
+
+export const { resetUserSlice } = userSlice.actions;
+
+export default userSlice.reducer;
