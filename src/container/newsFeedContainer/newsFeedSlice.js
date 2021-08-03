b524fb40624f313cca625968db9f5f0c61069e50
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-04 05:17:37 +0530

    deleted redundant reducer from newsFeed slice

--- src/container/newsFeedContainer/newsFeedSlice.js
+++ src/container/newsFeedContainer/newsFeedSlice.js
@@ -1,41 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apiCall } from "../../services/apiCall";
 import { current } from "immer";
-import { removePostLocal } from "../../localStorage";
 
 export let getAllPosts = createAsyncThunk(
   "posts/getAllPosts",
   async (userId, { fulfillWithValue, rejectWithValue }) => {
-    let response = await apiCall("GET", `post/`);
-
-    if (response.success) {
-      return fulfillWithValue(response);
-    } else {
-      return rejectWithValue(response);
-    }
-  }
-);
-
-export const addCommentFunction = createAsyncThunk(
-  "posts/addCommentFunction",
-
-  async ({ postId, text }, { fulfillWithValue, rejectWithValue }) => {
-    let response = await apiCall("POST", `comment/${postId}`, {
-      text: text,
-    });
-
-    if (response.success) {
-      return fulfillWithValue(response);
-    } else {
-      return rejectWithValue(response);
-    }
-  }
-);
-
-export const addPostToNewsFeed = createAsyncThunk(
-  "posts/addPostToNewsFeed",
-  async (postId, { fulfillWithValue, rejectWithValue }) => {
-    let response = await apiCall("GET", `post/${postId}`);
+    let response = await apiCall("GET", `post`);
 
     if (response.success) {
       return fulfillWithValue(response);
@@ -105,46 +75,6 @@ export const postSlice = createSlice({
     },
     [getAllPosts.rejected]: (state, action) => {
       state.status = "rejected";
-      state.message = action.payload.message;
-    },
-
-    [addCommentFunction.pending]: (state) => {
-      state.commentStatus = "loading";
-    },
-    [addCommentFunction.fulfilled]: (state, action) => {
-      let updatedPost = action.payload.data.post;
-      let updatedPostId = action.payload.data.post._id;
-
-      state.posts = state.posts.map((item) => {
-        if (item._id === updatedPostId) {
-          return updatedPost;
-        }
-        return item;
-      });
-
-      state.userPosts = state.userPosts.map((item) => {
-        if (item._id === updatedPostId) {
-          return updatedPost;
-        }
-        return item;
-      });
-
-      state.commentStatus = "fullfilled";
-    },
-    [addCommentFunction.rejected]: (state, action) => {
-      state.commentStatus = "rejected";
-    },
-
-    [addPostToNewsFeed.pending]: (state) => {
-      state.addPosttoNewsFeedStatus = "loading";
-    },
-    [addPostToNewsFeed.fulfilled]: (state, action) => {
-      state.addPosttoNewsFeedStatus = "fullfilled";
-      state.posts.unshift(action.payload.data.post);
-      state.message = action.payload.data.message;
-    },
-    [addPostToNewsFeed.rejected]: (state, action) => {
-      state.addPosttoNewsFeedStatus = "rejected";
       state.message = action.payload.message;
     },
   },
