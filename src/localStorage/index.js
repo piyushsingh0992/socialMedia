b34ae884bcdb6c87b77ffaeefe02e7c10c83e3a2
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-04 21:11:01 +0530

    deleted redundant functions from local Storage

--- src/localStorage/index.js
+++ src/localStorage/index.js
@@ -8,34 +8,6 @@ export function logOutLocal() {
   localStorage.removeItem("userDetails");
 }
 
-export function addFollowingLocal(followerId) {
-  let userDetails = JSON.parse(localStorage.getItem("userDetails"));
-  userDetails.userDetails.following.push(followerId);
-  localStorage.setItem("userDetails", JSON.stringify(userDetails));
-}
-
-export function removeFollowingLocal(followerId) {
-  let userDetails = JSON.parse(localStorage.getItem("userDetails"));
-  userDetails.userDetails.following = userDetails.userDetails.following.filter(
-    (item) => item != followerId
-  );
-  localStorage.setItem("userDetails", JSON.stringify(userDetails));
-}
-
-export function addPostLocal(postId) {
-  let userDetails = JSON.parse(localStorage.getItem("userDetails"));
-  userDetails.userDetails.posts.push(postId);
-  localStorage.setItem("userDetails", JSON.stringify(userDetails));
-}
-
-export function removePostLocal(postId) {
-  let userDetails = JSON.parse(localStorage.getItem("userDetails"));
-  userDetails.userDetails.posts = userDetails.userDetails.posts.filter(
-    (item) => item != postId
-  );
-  localStorage.setItem("userDetails", JSON.stringify(userDetails));
-}
-
 export function getLastRoute() {
   return localStorage.getItem("lastRoute");
 }
