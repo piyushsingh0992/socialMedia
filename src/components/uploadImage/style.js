c4a1115394fb56be6ed5a08cf46cd2d31298f9e7
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-05 03:49:01 +0530

    deleted redundant code from upload image

--- src/components/uploadImage/style.js
+++ src/components/uploadImage/style.js
@@ -34,43 +34,4 @@ export const useStyles = makeStyles((theme) => ({
   icon: {
     color: "white",
   },
-  paper: {
-    position: "absolute",
-    minWidth: 450,
-    backgroundColor: theme.palette.background.paper,
-    border: `2px solid ${theme.palette.primary.main}`,
-    boxShadow: theme.shadows[5],
-    borderRadius: "5px",
-    padding: theme.spacing(1, 1, 1),
-  },
-  formControl: {
-    minWidth: 120,
-    width: "100%",
-    padding: 0,
-    margin: 0,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-  card: {
-    margin: "1.5rem 0",
-  },
-  media: {
-    height: 0,
-    maxHeight: "350vh",
-    paddingTop: "56.25%", // 16:9
-  },
-  inputRoot: {
-    color: "inherit",
-
-    width: "100%",
-  },
-  inputInput: {
-    margin: theme.spacing(1, 1, 1, 1),
-    // vertical padding + font size from searchIcon
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-    padding: theme.spacing(1, 1, 1, 1),
-  },
 }));
