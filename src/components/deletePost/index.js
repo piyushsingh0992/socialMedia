ca45b2220141abe46bd60f24de9d689d2727e1e5
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-09 04:05:05 +0530

    added shareable link feature

--- src/components/deletePost/index.js
+++ src/components/deletePost/index.js
@@ -1,30 +1,28 @@
 import React from "react";
 import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
+
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import { deleteCurrentPost } from "../../container/postContainer/postSlice";
 import { useDispatch } from "react-redux";
-
-const DeletePost = ({ anchorEl, handleClose, postId,deleteLoaderSetter }) => {
+import Typography from "@material-ui/core/Typography";
+const DeletePost = ({ anchorEl, handleClose, postId, deleteLoaderSetter }) => {
   const dispatch = useDispatch();
 
   return (
-    <Menu
-      id="simple-menu"
-      anchorEl={anchorEl}
-      keepMounted
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-      onClick={() => {
-        deleteLoaderSetter(true);
-        dispatch(deleteCurrentPost(postId));
-      }}
-    >
-      <MenuItem onClick={handleClose}>
+    
+      <MenuItem
+        onClick={() => {
+          handleClose();
+          deleteLoaderSetter(true);
+          dispatch(deleteCurrentPost(postId));
+        }}
+      >
         <DeleteOutlineIcon />
-        Delete
+        &nbsp;&nbsp;
+              <Typography color="textSecondary" variant="p">
+        Delete</Typography>
       </MenuItem>
-    </Menu>
+    
   );
 };
 
--- src/components/postHeader/index.js
+++ src/components/postHeader/index.js
@@ -7,6 +7,12 @@ import DeletePost from "../deletePost";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ShareIcon from "@material-ui/icons/Share";
+import { toast } from "react-toastify";
+import Typography from "@material-ui/core/Typography";
+import RemoveFromQueueIcon from "@material-ui/icons/RemoveFromQueue";
 const PostHeader = ({
   handleClick,
   handleClose,
@@ -20,6 +26,14 @@ const PostHeader = ({
   let auth = useSelector((state) => state.auth);
   const navigate = useNavigate();
   let currentPost = useSelector((state) => state.post);
+
+  function copyToClipBoard() {
+    navigator.clipboard.writeText(
+      `https://social-1997.netlify.app/post/${postId}`
+    );
+
+    toast.success("copied to clipboard");
+  }
 
   return (
     <CardHeader
@@ -41,25 +55,59 @@ const PostHeader = ({
         borderBottom: "0.1px solid grey",
       }}
       action={
-        userDetails._id === auth.userKey && (
-          <div>
-            {deleteLoader ? (
-              <CircularProgress />
-            ) : (
-              <IconButton aria-label="settings" onClick={handleClick}>
-                <MoreVertIcon />
-              </IconButton>
+        <div>
+          {deleteLoader ? (
+            <CircularProgress />
+          ) : (
+            <IconButton aria-label="settings" onClick={handleClick}>
+              <MoreVertIcon />
+            </IconButton>
+          )}
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            onClick={() => {}}
+          >
+            <MenuItem
+              onClick={() => {
+                copyToClipBoard();
+                handleClose();
+              }}
+            >
+              <ShareIcon />
+              &nbsp;&nbsp;
+              <Typography color="textSecondary" variant="p">
+                Copy Link
+              </Typography>
+            </MenuItem>
+
+            <MenuItem
+              onClick={() => {
+                navigate(`/post/${postId}`);
+                handleClose();
+              }}
+            >
+              <RemoveFromQueueIcon />
+              &nbsp;&nbsp;
+              <Typography color="textSecondary" variant="p">
+                View Full screen
+              </Typography>
+            </MenuItem>
+            {userDetails._id === auth.userKey && (
+              <DeletePost
+                anchorEl={anchorEl}
+                handleClose={handleClose}
+                postId={postId}
+                userId={userDetails._id}
+                handleClick={handleClick}
+                deleteLoaderSetter={deleteLoaderSetter}
+              />
             )}
-            <DeletePost
-              anchorEl={anchorEl}
-              handleClose={handleClose}
-              postId={postId}
-              userId={userDetails._id}
-              handleClick={handleClick}
-              deleteLoaderSetter={deleteLoaderSetter}
-            />
-          </div>
-        )
+          </Menu>
+        </div>
       }
     />
   );
