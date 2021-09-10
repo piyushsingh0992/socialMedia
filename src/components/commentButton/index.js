b62eeba0e90225a399a6921f649b6a76eaa24a0b
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-11 01:51:26 +0530

    added toaster to comment button

--- src/components/commentButton/index.js
+++ src/components/commentButton/index.js
@@ -3,13 +3,20 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
 import { useStyles } from "./style.js";
-
-const CommentButton = ({ handleExpandClick ,comment}) => {
+import { toast } from "react-toastify";
+const CommentButton = ({ handleExpandClick, comment }) => {
   const classes = useStyles();
+
+  function handleClick() {
+    comment && comment > 0
+      ? handleExpandClick()
+      : toast.success("No comments to display");
+  }
+
   return (
-    <IconButton aria-label="share" onClick={handleExpandClick}>
+    <IconButton aria-label="share" onClick={handleClick}>
       <Typography color="textSecondary" variant="p">
-        {comment} 
+        {comment}
       </Typography>
       <ChatBubbleOutlineOutlinedIcon className={classes.icon} />
     </IconButton>
